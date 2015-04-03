a39a4bb509eca308812557b10fcb718d13c0e4da
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-03 10:27:33 -0400

    Add Workspace.{open, reopenItem, addOpener}

--- lib/src/workspace.dart
+++ lib/src/workspace.dart
@@ -11,6 +11,25 @@ class Workspace {
   Disposable observeTextEditors(Consumer<TextEditor> callback) {
     return new Disposable(obj.callMethod("observeTextEditors", [(o) => callback(new TextEditor.wrap(o))]));
   }
+
+  Future<TextEditor> open(String uri, {int initialLine, int initialColumn, String split, bool activatePane, bool searchAllPanes}) async {
+    var map = omap({
+      "initialLine": initialLine,
+      "initialColumn": initialColumn,
+      "split": split,
+      "activatePane": activatePane,
+      "searchAllPanes": searchAllPanes
+    });
+
+    return new TextEditor.wrap(await promiseToFuture(
+      obj.callMethod("open", [uri, map])
+    ));
+  }
+
+  Future reopenItem() => promiseToFuture(obj.callMethod("reopenItem"));
+  Disposable addOpener(callback(String uri)) {
+    return new Disposable(obj.callMethod("addOpener", [callback]));
+  }
 }
 
 typedef void Consumer<T>(T input);
