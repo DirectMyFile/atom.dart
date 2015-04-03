4c308109bd413f1c206185803fd1333db8ebce3c
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-03 10:23:12 -0400

    Implement Most of GitRepository

--- lib/src/project.dart
+++ lib/src/project.dart
@@ -38,6 +38,42 @@ class GitRepository {
   GitRepository(this.obj);
 
   String get type => obj.callMethod("getType");
+  String get path => obj.callMethod("getPath");
+  String get workingDirectory => obj.callMethod("getWorkingDirectory");
+  bool get isProjectAtRoot => obj.callMethod("isProjectAtRoot");
+  String relativize() => obj.callMethod("relativize");
+  bool get hasBranch => obj.callMethod("hasBranch");
+  String getShortHead(String path) => obj.callMethod("getShortHead", [path]);
+  bool isSubmodule(String path) => obj.callMethod("isSubmodule", [path]);
+  int getAheadBehindCount(String ref, String path) => obj.callMethod("getAheadBehindCount", [ref, path]);
+  int getCachedUpstreamAheadBehindCount(String path) => obj.callMethod("getCachedUpstreamAheadBehindCount", [path]);
+  dynamic getConfigValue(String path) => obj.callMethod("getConfigValue", [path]);
+  String getOriginUrl([String path]) => obj.callMethod("getOriginURL", [path]);
+  String getUpstreamBranch(String path) => obj.callMethod("getUpstreamBranch", [path]);
+  GitReferences getReferences(String path) => new GitReferences(obj.callMethod("getReferences", [path]));
+
+  bool isPathModified(String path) => obj.callMethod("isPathModified", [path]);
+  bool isPathNew(String path) => obj.callMethod("isPathNew", [path]);
+  bool isPathIgnored(String path) => obj.callMethod("isPathIgnored", [path]);
+  int getDirectoryStatus(String path) => obj.callMethod("getDirectoryStatus", [path]);
+  int getPathStatus(String path) => obj.callMethod("getPathStatus", [path]);
+  int getCachedPathStatus(String path) => obj.callMethod("getCachedPathStatus", [path]);
+  bool isStatusModified(int status) => obj.callMethod("isStatusModified", [status]);
+  bool isStatusNew(int status) => obj.callMethod("isStatusNew", [status]);
+  bool checkoutHead(String path) => obj.callMethod("checkoutHead", [path]);
+  bool checkoutReference(String reference, [bool create = true]) {
+    return obj.callMethod("checkoutReference", [reference, create]);
+  }
+}
+
+class GitReferences {
+  final js.JsObject obj;
+
+  GitReferences(this.obj);
+
+  List<String> get heads => obj["heads"];
+  List<String> get remotes => obj["remotes"];
+  List<String> get tags => obj["tags"];
 }
 
 class Directory {
